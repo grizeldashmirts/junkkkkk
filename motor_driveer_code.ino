7381d31e55b8dc586a97f083b886f8d7f1f5c83d
Grizeldashmirts <guyros20541@gmail.com>
2026-04-27 11:51:03 +0300

    with gal
    Initialize motor control pins and define motGO function.

---
+++ motor_driveer_code.ino
@@ -0,0 +1,19 @@
+  int in1 = 5;
+  int in2 = 6;
+
+  void motGO(int speed){
+  // speed between -100 and 100
+  // with map function
+  // absolute value
+  }
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(in1, OUTPUT);
+  pinMOde(in2, OUTPUT)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+}
