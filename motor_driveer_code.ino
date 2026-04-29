e2322861b7237f587bc57edc96e768dfa97640ae
Grizeldashmirts <guyros20541@gmail.com>
2026-04-29 15:46:32 +0300

    Implement speed validation in motGO function
    Added speed validation and error message in motGO function.

--- motor_driveer_code.ino
+++ motor_driveer_code.ino
@@ -1,10 +1,24 @@
   int in1 = 5;
   int in2 = 6;
-
   void motGO(int speed){
   // speed between -100 and 100
   // with map function
   // absolute value
+  if(speed < -100 || speed > 100){
+  serial.println("unvalid speed")
+  }
+  else{
+    if(speed == 0){
+      
+    }
+    else if(speed < 0){
+
+    }
+    else{
+
+    }
+  }
+
   }
 
 void setup() {
