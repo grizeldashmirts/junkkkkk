d68a1aba64f773585b614105087890e0776eed3b
Grizeldashmirts <guyros20541@gmail.com>
2026-05-04 11:23:05 +0300

    with gal

--- motor_driveer_code.ino
+++ motor_driveer_code.ino
@@ -1,11 +1,11 @@
   int in1 = 5;
   int in2 = 6;
-  void motGO(int speed){
+  void motGo(int speed){
   // speed between -100 and 100
   // with map function
   // absolute value
   if(speed < -100 || speed > 100){
-  speed = constrain(speed, -100, 100)
+  speed = constrain(speed, -100, 100);
   }
     if(speed == 0){
       analogWrite(in1, 0);
@@ -28,18 +28,18 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(in1, OUTPUT);
-  pinMOde(in2, OUTPUT);
+  pinMode(in2, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- motGo(50)
- delay(1000)
- motGo(150)
- delay(1000)
- motGo(-50)
- delay(1000)
- motGo(-150)
- delay(1000)
+ motGo(50);
+ delay(1000);
+ motGo(150);
+ delay(1000);
+ motGo(-50);
+ delay(1000);
+ motGo(-150);
+ delay(1000);
 }
