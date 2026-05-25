7e948d6014bee0f4e6a738732f4ab804b5f76aa8
Grizeldashmirts <guyros20541@gmail.com>
2026-05-25 11:56:26 +0300

    with gal

--- motor_driveer_code.ino
+++ motor_driveer_code.ino
@@ -1,45 +1,59 @@
-  int in1 = 5;
-  int in2 = 6;
-  void motGo(int speed){
+int in1 = 5;
+int in2 = 6;
+int trig = 10;
+int echo = 9;
+int getDist() {
+  digitalWrite(trig, LOW);
+  delay(2);
+  digitalWrite(trig, HIGH);
+  delay(10);
+  digitalWrite(trig, LOW);
+  float dur, dist;
+  dur = pulseIn(echo, HIGH);
+  dist = (dur * .0343) / 2;
+}
+void motGo(int speed) {
   // speed between -100 and 100
   // with map function
   // absolute value
-  if(speed < -100 || speed > 100){
-  speed = constrain(speed, -100, 100);
+  if (speed < -100 || speed > 100) {
+    speed = constrain(speed, -100, 100);
   }
-    if(speed == 0){
-      analogWrite(in1, 0);
-      analogWrite(in2, 0);
-    }
-    else if(speed > 0){
-      speed = map(speed, 0, 100, 0, 255);
-      analogWrite(in1, speed);
-      analogWrite(in2, 0);
-    }
-    else{
-      speed = abs(speed);
-      speed = map(speed, 0, 100, 0, 255);
-      analogWrite(in1, 0);
-      analogWrite(in2, speed);
-    }
-    Serial.println(speed);
+  if (speed == 0) {
+    analogWrite(in1, 0);
+    analogWrite(in2, 0);
+  } else if (speed > 0) {
+    speed = map(speed, 0, 100, 0, 255);
+    analogWrite(in1, speed);
+    analogWrite(in2, 0);
+  } else {
+    speed = abs(speed);
+    speed = map(speed, 0, 100, 0, 255);
+    analogWrite(in1, 0);
+    analogWrite(in2, speed);
   }
+  Serial.println(speed);
+}
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMOde(echo, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- motGo(50);
- delay(1000);
- motGo(150);
- delay(1000);
- motGo(-50);
- delay(1000);
- motGo(-150);
- delay(1000);
+  motGo(50);
+  delay(1000);
+  motGo(150);
+  delay(1000);
+  motGo(0);
+  delay(1000);
+  motGo(-50);
+  delay(1000);
+  motGo(-150);
+  delay(1000);
 }
